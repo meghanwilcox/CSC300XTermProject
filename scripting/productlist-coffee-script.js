4e27af520774a2b9987458a1e78894fbcb7da885
meghan wilcox <meghanwilcox4235@gmail.com>
2024-02-13 16:53:46 -0800

    added buttons + functionality to filter by price

--- scripting/productlist-coffee-script.js
+++ scripting/productlist-coffee-script.js
@@ -3,15 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
     
     // the products to an array
     const products = [
+        { name: "Test Product", cost: 1.00},
+        { name: "Test Product", cost: 2.00},
+        { name: "Test Product", cost: 3.00},
+        { name: "Test Product", cost: 4.00},
         { name: "Test Product", cost: 5.00},
-        { name: "Test Product", cost: 5.00},
-        { name: "Test Product", cost: 5.00},
-        { name: "Test Product", cost: 5.00},
-        { name: "Test Product", cost: 5.00},
-        { name: "Test Product", cost: 5.00},
-        { name: "Test Product", cost: 5.00},
-        { name: "Test Product", cost: 5.00},
-        { name: "Test Product", cost: 5.00}
+        { name: "Test Product", cost: 6.00},
+        { name: "Test Product", cost: 7.00},
+        { name: "Test Product", cost: 9.00},
+        { name: "Test Product", cost: 8.00}
     ];
 
     function updateProductsDisplayed() {
@@ -33,6 +33,33 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
+    function sortProductsLowToHigh() {
+        const productsContainer = document.getElementById('product-list-container');
+
+        productsContainer.innerHTML = "";
+        
+        products.sort((a, b) => a.cost - b.cost);
+
+        updateProductsDisplayed();
+
+    }
+
+    function sortProductsHighToLow() {
+        const productsContainer = document.getElementById('product-list-container');
+
+        productsContainer.innerHTML = "";
+
+        products.sort((a, b) => b.cost - a.cost);
+
+        updateProductsDisplayed();
+    }
+
     updateProductsDisplayed();
+
+    const lowToHighButton = document.getElementById('low-to-high');
+    lowToHighButton.addEventListener('click', sortProductsLowToHigh);
+
+    const highToLowButton = document.getElementById('high-to-low');
+    highToLowButton.addEventListener('click', sortProductsHighToLow);
 
 });
