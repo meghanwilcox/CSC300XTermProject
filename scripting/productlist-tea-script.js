38eabf6a6d96f0d879672be076b6cc1af074afe1
meghan wilcox <meghanwilcox4235@gmail.com>
2024-02-25 15:35:11 -0800

    created module system for product array

--- scripting/productlist-tea-script.js
+++ scripting/productlist-tea-script.js
@@ -1,35 +1,28 @@
+// import the producst array 
+import { products } from "./index.js";
+
 //this function will be called as soon as the page is loaded
-document.addEventListener('DOMContentLoaded', function() {
-    
-    // the products to an array
-    const products = [
-        { name: "Test Product", cost: 1.00},
-        { name: "Test Product", cost: 6.00},
-        { name: "Test Product", cost: 3.00},
-        { name: "Test Product", cost: 5.00},
-        { name: "Test Product", cost: 4.00},
-        { name: "Test Product", cost: 2.00},
-        { name: "Test Product", cost: 7.00},
-        { name: "Test Product", cost: 9.00},
-        { name: "Test Product", cost: 8.00}
-    ];
+document.addEventListener('DOMContentLoaded', function () {
 
     function updateProductsDisplayed() {
         const productsContainer = document.getElementById('product-list-container');
 
         products.forEach((item, index) => {
 
-            const productDivElement = document.createElement('div');
+            if (item.category === "tea") {
+                const productDivElement = document.createElement('div');
 
-            productDivElement.classList.add('products');
+                productDivElement.classList.add('products');
 
-            productDivElement.innerHTML = `
-            <img class="photo" data-index="${index}" src="images/${item.name.toLowerCase()}.jpg" alt="">
-            <p>${item.name}<br>
-                $${item.cost.toFixed(2)}
-            </p>`;
+                productDivElement.innerHTML = `
+                    <img class="photo" data-index="${index}" src="images/${item.name.toLowerCase()}.jpg" alt="">
+                    <p>${item.name}<br>
+                        $${item.cost.toFixed(2)}
+                    </p>`;
 
-            productsContainer.appendChild(productDivElement);
+                productsContainer.appendChild(productDivElement);
+            }
+
         });
     }
 
